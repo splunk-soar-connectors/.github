c82f095f7adcfeae9631c5aab343ec60b3df0c48
mnordby-splunk <mnordby@splunk.com>
2025-02-18 14:11:12 -0700

    PAPP-35405 Security scans action (#160)
    * PAPP-35405 Security scans action
    
    * PAPP-35405 newline
    
    * PAPP-35405 python is already specified on codebuild
    
    * PAPP-35405 correct branch for scans
    
    * PAPP-35405 fix formatting
    
    * PAPP-35405 remove unused logging
    
    * PAPP-35405 remove spaces
    
    * PAPP-35405 updates for comments
    
    * PAPP-35405 change branch to main before merge

---
+++ .github/actions/security-scans/run_gitlab_pipeline.py
@@ -0,0 +1,72 @@
+"""
+Creates a run for a specified pipeline in GitLab and polls for its completion
+"""
+
+import argparse
+import json
+import logging
+
+import backoff
+
+from utils.api.gitlab import GitLabApi
+
+
+def create_arg_parser():
+    help_str = " ".join(line.strip() for line in __doc__.splitlines())
+    argparser = argparse.ArgumentParser(description=help_str)
+    argparser.add_argument(
+        "repo_name", type=str, help="Name of the GitLab repo to create a pipeline run for"
+    )
+    argparser.add_argument("repo_branch", type=str, help="Branch of the repo to use")
+    argparser.add_argument(
+        "-v",
+        "--pipeline-vars",
+        type=str,
+        help="""JSON formatted string of variables to pass to the pipeline,
+                                   eg, \'{"key1": "val1", "key2", "val2"}\'""",
+    )
+    argparser.add_argument(
+        "-t", "--timeout", type=int, default=1200, help="Max time in seconds to wait for completion"
+    )
+    argparser.add_argument(
+        "-o", "--out", type=str, default="pipeline_run.json", help="File to write run details"
+    )
+    return argparser
+
+
+@backoff.on_predicate(backoff.constant, interval=10, max_time=1200, jitter=None)
+def _poll_pipeline_completion(gitlab_client, repo_name, pipeline_id):
+    run_details = gitlab_client.get_pipeline_run(repo_name, pipeline_id)
+    status = run_details["status"]
+
+    if status in {"created", "pending", "running"}:
+        logging.info("Pipeline status %s, retrying...", status)
+        return None
+
+    return run_details
+
+
+def main(**kwargs):
+    gitlab = GitLabApi()
+    create_pipeline_resp = gitlab.create_pipeline_run(
+        kwargs["repo_name"], kwargs["repo_branch"], **json.loads(kwargs["pipeline_vars"])
+    )
+    logging.info("Created pipeline run %s", repr(create_pipeline_resp))
+
+    completion_resp = _poll_pipeline_completion(
+        gitlab, kwargs["repo_name"], create_pipeline_resp["id"]
+    )
+    logging.info("Pipeline completed with response %s", repr(completion_resp))
+
+    with open(kwargs["out"], "w") as out:
+        json.dump(completion_resp, out)
+        logging.info("Results written to %s", out.name)
+
+    exit(0 if completion_resp["status"] == "success" else 1)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    parser = create_arg_parser()
+    options = vars(parser.parse_args())
+    main(**options)
---
+++ .github/actions/security-scans/utils/api/gitlab.py
@@ -0,0 +1,92 @@
+import os
+
+from utils.api import ApiSession
+from urllib.parse import quote_plus
+
+GITLAB_SERVER = "cd.splunkdev.com"
+PH_GRP_NAME = "phantom"
+PH_REPO_NAME = "phantom"
+APP_GRP_NAME = "phantom apps"
+QA_REPO_NAME = "qa"
+GITLAB_API_TOKEN = os.environ.get("GITLAB_API_TOKEN")
+
+SKIPPED_REPOS = {
+    "phantom/assets"  # Deprecated repo to be deleted in the future
+}
+
+
+class GitLabApi:
+    """
+    Our internal GitLab utils class.
+    """
+
+    __initialized = None
+
+    grp_ids = {}
+    proj_ids = {}
+
+    def __init__(self, token=None):
+        self.session = ApiSession(f"https://{GITLAB_SERVER}/api/v4")
+
+        token = token if token else GITLAB_API_TOKEN
+        assert token, f"Invalid value given as GitLab API token: {token}"
+        self.session.headers.update({"Private-Token": token})
+
+        # Only need the rest if we're using a new token
+        if GitLabApi.__initialized != token:
+            # Get groups information
+            for group in self.iter_groups(search="phantom"):
+                self.grp_ids[group["full_name"].lower()] = group["id"]
+
+            # Save project ID's for the important groups
+            for group in (PH_GRP_NAME, APP_GRP_NAME):
+                for project in self.iter_repos(group):
+                    if project["path_with_namespace"] in SKIPPED_REPOS:
+                        continue
+                    self.proj_ids[project["name"].lower()] = quote_plus(
+                        project["path_with_namespace"]
+                    )
+
+        GitLabApi.__initialized = token
+
+    def _iter_data(self, url, **kwargs):
+        while url is not None:
+            resp = self.session.get(url, params=kwargs.pop("params", None), **kwargs)
+            page = resp.json()
+            if isinstance(page, list):
+                yield from page
+            else:
+                yield page
+
+            try:
+                url = resp.links["next"]["url"]
+            except KeyError:
+                url = None
+
+    def iter_groups(self, search=None):
+        params = {}
+        if search:
+            params["search"] = search
+        yield from self._iter_data("/groups", params=params)
+
+    def iter_repos(self, group_name=APP_GRP_NAME):
+        group_id = self.grp_ids[group_name.lower()]
+        yield from self._iter_data(f"/groups/{group_id}/projects", params={"per_page": 50})
+
+    def create_pipeline_run(self, repo_name, git_ref, **pipeline_vars):
+        """
+        Creates a pipeline run for the given repo/branch using the given
+        variables
+        """
+        repo_id = self.proj_ids[repo_name.lower()]
+        req_body = {"variables": [{"key": k, "value": v} for k, v in pipeline_vars.items()]}
+        return self.session.post(
+            f"/projects/{repo_id}/pipeline?ref={git_ref}", json=req_body
+        ).json()
+
+    def get_pipeline_run(self, repo_name, pipeline_id):
+        """
+        Fetches details for a given pipeline run
+        """
+        repo_id = self.proj_ids[repo_name.lower()]
+        return self.session.get(f"/projects/{repo_id}/pipelines/{pipeline_id}").json()
