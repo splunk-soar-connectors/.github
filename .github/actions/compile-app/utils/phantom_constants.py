e2b7aef1576e60e769c16e1c0046a0fd0a441d45
mnordby-splunk <mnordby@splunk.com>
2025-02-19 10:04:43 -0600

    phantom vars

--- .github/actions/compile-app/utils/phantom_constants.py
+++ .github/actions/compile-app/utils/phantom_constants.py
@@ -4,11 +4,11 @@ DIR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
 os.chdir(DIR)
 
 PHANTOM_INSTANCE_CURRENT_VERSION_IP = os.getenv(
-    "PHANTOM_INSTANCE_CURRENT_VERSION_IP", "1.1.1.1"
+    "PHANTOM_INSTANCE_CURRENT_VERSION_IP", "10.1.65.241"
 )
 PHANTOM_INSTANCE_PREVIOUS_VERSION_IP = os.getenv(
-    "PHANTOM_INSTANCE_PREVIOUS_VERSION_IP", "1.1.1.1"
+    "PHANTOM_INSTANCE_PREVIOUS_VERSION_IP", "10.1.18.235"
 )
 
-PHANTOM_SSH_USER = os.getenv("PHANTOM_SSH_USER", "sshuser")
-PHANTOM_PASSWORD = os.getenv("PHANTOM_PASSWORD", "hunter2")
+PHANTOM_SSH_USER = os.getenv("PHANTOM_SSH_USER", "phantom")
+PHANTOM_PASSWORD = os.getenv("PHANTOM_PASSWORD", "password")
