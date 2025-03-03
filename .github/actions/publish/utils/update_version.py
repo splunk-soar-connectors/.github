1cb2e00cef38a0b8d6e951f8626ee946f311b6fa
mnordby-splunk <mnordby@splunk.com>
2025-03-03 14:41:54 -0700

    PAPP-35411 update script

--- .github/actions/publish/utils/update_version.py
+++ .github/actions/publish/utils/update_version.py
@@ -1,7 +1,10 @@
 # This script should find the instances where we store the app version in code and update them (app.json, README.md, etc...)
 import os
-
-print("Called update_version.py")
+import json
+from collections import OrderedDict
+from datetime import datetime, timezone
+import argparse
+import re
 
 def find_app_json_name(json_filenames):
     """
@@ -18,16 +21,61 @@ def find_app_json_name(json_filenames):
         filtered_json_filenames.append(fname)
 
     if len(filtered_json_filenames) == 0:
-        raise ValueError("No JSON file found in top level of app repo! Aborting tests...")
+        print("No JSON file found in top level of app repo! Aborting tests...")
+        exit(1)
 
     if len(filtered_json_filenames) > 1:
-        raise ValueError(
+        print(
             f"Multiple JSON files found in top level of app repo: {filtered_json_filenames}."
             "Aborting because there should be exactly one top level JSON file."
         )
+        exit(1)
 
     # There's only one json file in the top level, so it must be the app's json
     return filtered_json_filenames[0]
 
-main_app_json_name = find_app_json_name([f for f in os.listdir(os.getcwd()) if f.endswith('.json')])
-print(main_app_json_name)
+def create_cmdline_parser():
+    """
+    Commandline parser for passing in necessary arguments
+    """
+    help_str = " ".join(line.strip() for line in __doc__.splitlines())
+    argparser = argparse.ArgumentParser(description=help_str)
+    argparser.add_argument(
+        "new_version", type=str, help="The new version the app json will be updated to"
+    )
+
+    return argparser
+
+def main(**kwargs):
+
+    if not kwargs.get("new_version") or not re.match(r'^\d+\.\d+\.\d+$', kwargs.get("new_version")):
+        print(f'New version provided by semantic-release is not formatted correctly: {kwargs.get("new_version")}')
+        exit(1)
+
+    new_version = kwargs.get("new_version")
+
+    main_app_json_name = find_app_json_name([f for f in os.listdir(os.getcwd()) if f.endswith('.json')])
+    print(f"Found one top-level json file: {main_app_json_name}")
+
+    # First, determine indent level of app json
+    indent = ""
+    with open(main_app_json_name) as f:
+        for line in f:
+            if "appid" in line:
+                indent = re.match(r"(\s*)", line).group(0)
+                break
+
+    with open(main_app_json_name, 'r') as f:
+        json_content = json.loads(f.read(), object_pairs_hook=OrderedDict)
+    json_content["app_version"] = new_version
+    # Update the last update time
+    json_content["utctime_updated"] = datetime.now(tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    with open(main_app_json_name, 'w') as f:
+        json.dump(json_content, f, indent=len(indent), sort_keys=False, separators=(",", ": "))
+
+
+
+if __name__ == "__main__":
+    parser = create_cmdline_parser()
+    options = vars(parser.parse_args())
+    main(**options)
