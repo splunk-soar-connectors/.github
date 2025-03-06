03e31707a9f9c267154131b50d53a07ab8535e46
mnordby-splunk <mnordby@splunk.com>
2025-03-06 10:46:29 -0700

    Publish workflow with semantic_release, build_app, and upload_to_splunkbase actions (#164)
    Semantic-release and dependencies are installed
    Files are copied to the app repo for semantic release to run (and are removed before build)
    Semantic release runs with custom exec command which updates app version and generates release notes in our existing format
    Semantic release commits these changes automatically, adds comments to relevant PRs, and creates a release tag in Git
    Build_app is used to create a tarball for the new app (which is now stored as a github workflow run artifact instead of in s3)
    New Publish action uses the tarball artifact, publishes it to Splunkbase, and sends a release notification to Slack

---
+++ .github/actions/publish/upload_to_splunkbase.py
@@ -0,0 +1,162 @@
+"""
+Uploads a version of an app to Splunkbase
+"""
+
+import argparse
+import json
+import logging
+import os
+import sys
+import tarfile
+from packaging.version import parse
+
+import boto3
+
+from utils.update_version import find_app_json_name
+from utils.api.splunkbase import (
+    APACHE2_LICENSE_STRING,
+    APACHE2_LICENSE_URL,
+    SGT_LICENSE_STRING,
+    SGT_LICENSE_URL,
+    Splunkbase,
+)
+
+NEW_APP_WARNING_MESSAGE = (
+    "Successfully uploaded a NEW APP to Splunkbase. "
+    "Please notify the Splunkbase team. "
+    "See: http://go/new-soar-app-in-splunkbase for more."
+)
+
+RELEASE_QUEUE_URL = os.getenv("RELEASE_QUEUE_URL")
+SOAR_APPS_TOKEN = os.getenv("SOAR_APPS_TOKEN")
+SPLUNKBASE_USER = os.getenv("SPLUNKBASE_USER")
+SPLUNKBASE_PASSWORD = os.getenv("SPLUNKBASE_PASSWORD")
+RELEASE_QUEUE_REGION = 'us-west-2'
+
+
+
+def parse_args() -> argparse.Namespace:
+    help_str = " ".join(line.strip() for line in (__doc__ or "").splitlines())
+    parser = argparse.ArgumentParser(description=help_str)
+    parser.add_argument("app_repo_name", help="Name of the app's GitHub repo.")
+    return parser.parse_args()
+
+
+def get_release_notes(tarball, version):
+    with tarfile.open(tarball, "r") as tar:
+        filename = f"release_notes/{version}.md"
+        names = tar.getnames()
+        notes_for_version = [n for n in names if filename in n]
+        if notes_for_version:
+            return tar.extractfile(notes_for_version[0]).read()
+    return None
+
+
+def get_app_json(tarball):
+    with tarfile.open(tarball, "r") as tar:
+        names = tar.getnames()
+        app_json_files = [n for n in names if n.endswith(".json") and n.count("/") == 1]
+        app_json_name = find_app_json_name(app_json_files)
+        app_json = tar.extractfile(app_json_name).read()
+    return json.loads(app_json)
+
+
+def get_license_info(app_json):
+    if app_json["publisher"] == "Splunk":
+        return (SGT_LICENSE_STRING, SGT_LICENSE_URL)
+
+    return (APACHE2_LICENSE_STRING, APACHE2_LICENSE_URL)
+
+
+def _send_release_message(repo_name, new_app, release_notes, app_json):
+    sqs = boto3.resource("sqs", region_name=RELEASE_QUEUE_REGION)
+    queue = sqs.Queue(RELEASE_QUEUE_URL)
+
+    message = {
+        "app_id": app_json["appid"],
+        "app_name": app_json["name"],
+        "app_logo": app_json["logo"],
+        "repo_name": repo_name,
+        "release_notes": release_notes.decode().split("\n"),
+        "release_version": app_json["app_version"],
+        "new_app": new_app,
+    }
+
+    queue.send_message(MessageBody=json.dumps(message))
+
+
+def main(args):
+    app_repo_name = args.app_repo_name 
+
+    tarball = os.getenv("UPLOAD_PATH")
+    logging.info("Downloaded tarball to %s", tarball)
+    app_json = get_app_json(tarball)
+    app_version = app_json["app_version"]
+    appid = app_json["appid"]
+
+    logging.info("Candidate version for release: %s", app_version)
+    sb_client = Splunkbase(SPLUNKBASE_USER, SPLUNKBASE_PASSWORD)
+
+    existing_releases = sb_client.get_existing_releases(appid)
+    if existing_releases:
+        latest_release = max(parse(r["release_name"]) for r in existing_releases)
+        logging.info("Latest released version: %s", latest_release.vstring)
+
+        if parse(app_version) <= latest_release:
+            logging.error(
+                "Candidate version %s must be greater than the latest released version %s",
+                app_version,
+                latest_release.vstring,
+            )
+            return 1
+    else:
+        logging.info("Version %s will be the first release", app_version)
+
+    release_notes = get_release_notes(tarball, app_version)
+    if not release_notes:
+        logging.error("Could not find release notes in tarball for version %s!", app_version)
+        return 1
+
+    logging.info("Found release notes for version %s: %s", app_version, release_notes)
+
+    license_string, license_url = get_license_info(app_json)
+    logging.info("Using license info: %s: %s", license_string, license_url)
+
+    apps = sb_client.get_apps({"appid": appid})
+    if apps:
+        sb_appid = apps[0]["id"]
+        logging.info("Found existing app with appid: %s: %s", appid, sb_appid)
+        package_id = sb_client.upload_app_version(
+            sb_appid, app_repo_name, tarball, release_notes, license_string, license_url
+        )
+    else:
+        logging.info("Could not find an app with appid: %s", appid)
+        package_id = sb_client.upload_app(
+            app_repo_name, tarball, release_notes, license_string, license_url
+        )
+
+    logging.info("Package ID: %s", package_id)
+    response = sb_client.check_upload_status(package_id)
+    sb_appid = response.get("details", {}).get("id")
+    if sb_appid:
+        logging.info("Upload validated successfully: \n%s", json.dumps(response, indent=2))
+    else:
+        logging.info("Failed to validate upload: \n%s", json.dumps(response, indent=2))
+        return 1
+
+    print(f"sending a release message with repo_name={app_repo_name}, new_app={not apps}")
+    _send_release_message(
+        repo_name=app_repo_name, new_app=not apps, app_json=app_json, release_notes=release_notes
+    )
+
+    if not apps:
+        sb_client.add_app_editor(sb_appid)
+        logging.warning(NEW_APP_WARNING_MESSAGE)
+        return 2
+
+    return 0
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    sys.exit(main(parse_args()))
---
+++ .github/actions/publish/utils/update_version.py
@@ -0,0 +1,115 @@
+# This script updates the app json with the new version provided by semantic release, and creates release notes for that release
+# Explored but currently not using: auto-generating release notes with ${nextRelease.notes} from semantic release
+import os
+import json
+import argparse
+import re
+from collections import OrderedDict
+from datetime import datetime, timezone
+
+def find_app_json_name(json_filenames):
+    """
+    Given a list of possible json files and the app repo name, return the name of the file
+    that is most likely to be the app repo's main module json
+    """
+    # Multiple json files. Exclude known JSON filenames and expect only one at the end regardless of name.
+    # Other places (e.g. Splunkbase) enforce a single top-level JSON file anyways.
+    filtered_json_filenames = []
+    for fname in json_filenames:
+        # Ignore the postman collection JSON files
+        if "postman_collection" in fname.lower():
+            continue
+        filtered_json_filenames.append(fname)
+
+    if len(filtered_json_filenames) == 0:
+        print("No JSON file found in top level of app repo! Aborting tests...")
+        exit(1)
+
+    if len(filtered_json_filenames) > 1:
+        print(
+            f"Multiple JSON files found in top level of app repo: {filtered_json_filenames}."
+            "Aborting because there should be exactly one top level JSON file."
+        )
+        exit(1)
+
+    # There's only one json file in the top level, so it must be the app's json
+    return filtered_json_filenames[0]
+
+def update_app_version_in_app_json(app_json_name: str, new_version: str):
+    # First, determine indent level of app json
+    indent = ""
+    with open(app_json_name) as f:
+        for line in f:
+            if "appid" in line:
+                indent = re.match(r"(\s*)", line).group(0)
+                break
+
+    # Extract existing json
+    with open(app_json_name, 'r') as f:
+        json_content = json.loads(f.read(), object_pairs_hook=OrderedDict)
+    
+    # Update values
+    json_content["app_version"] = new_version
+    json_content["utctime_updated"] = datetime.now(tz=timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    
+    print(f"Updating app json with new version: {new_version}")
+    # Write json back to file
+    with open(app_json_name, 'w') as f:
+        json.dump(json_content, f, indent=len(indent), sort_keys=False, separators=(",", ": "))
+        f.write('\n')
+
+
+def generate_release_notes(new_version: str) -> None:
+    print("Generating new release notes from unreleased.md")
+    # Get release notes from unreleased.md
+    with open("release_notes/unreleased.md", 'r') as f:
+        release_notes = f.read()
+
+    # Copy release notes to new version.md
+    with open(f"release_notes/{new_version}.md", 'w') as f:
+        f.write(f"** {new_version} **\n")
+        lines = release_notes.split("\n")
+        if len(lines) < 2:
+            print("Release notes not formatted correctly, not adding to official release notes")
+        else:
+            for line in lines[1:]:
+                f.write(line)
+
+    # Clear release notes from unreleased.md
+    with open("release_notes/unreleased.md", 'w') as f:
+        f.truncate(0)
+        f.write("** Unreleased **\n")
+
+def create_cmdline_parser():
+    """
+    Commandline parser for passing in necessary arguments
+    """
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "new_version", type=str, help="The new version the app json will be updated to"
+    )
+    argparser.add_argument(
+        "release_notes", type=str, help="The release notes for the new version"
+    )
+
+    return argparser
+
+def main(**kwargs):
+
+    if not kwargs.get("new_version") or not re.match(r'^\d+\.\d+\.\d+$', kwargs.get("new_version")):
+        print(f'New version provided by semantic-release is not formatted correctly: {kwargs.get("new_version")}')
+        exit(1)
+
+    new_version = kwargs.get("new_version")
+
+    app_json_name = find_app_json_name([f for f in os.listdir(os.getcwd()) if f.endswith('.json')])
+    print(f"Found one top-level json file: {app_json_name}")
+    
+    update_app_version_in_app_json(app_json_name, new_version)
+
+    generate_release_notes(new_version)
+
+if __name__ == "__main__":
+    parser = create_cmdline_parser()
+    options = vars(parser.parse_args())
+    main(**options)
